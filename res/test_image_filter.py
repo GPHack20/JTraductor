adde9dbb95ff18658aa292349dd473b80bba65bc
GUIDO PEÑALOZA <gsant@AdrilW.k503.d5.khai.edu>
2019-10-29 15:17:49 +0200

    Gitignore

--- res/test_image_filter.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image 
-image_file = Image.open("convert_image.png") # open colour image
-image_file = image_file.convert('1') # convert image to black and white
-image_file.save('result.png')
